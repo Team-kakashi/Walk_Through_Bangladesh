18440ff110ea5b053c6e3027682aef718ad82344
Shahriar Asfaq <shahriarasfaq@iut-dhaka.edu>
2022-01-16 16:15:25 +0600

    signup variable value save

--- Signup.js
+++ Signup.js
@@ -1,63 +1,120 @@
 import * as React from 'react';
 //import { TextInput } from 'react-native-paper';
-import { SafeAreaView, StyleSheet, TextInput, Text, Button } from "react-native";
+import {Picker} from '@react-native-picker/picker' ;
+import { TouchableOpacity, SafeAreaView, StyleSheet, TextInput, Text, Button, View } from "react-native";
 
 const Register = () => {
     const [name, setName] = React.useState('');
     const [contactno, setContactno] = React.useState('');
     const [email, setEmail] = React.useState('');
     const [password, setPassword] = React.useState('');
+    const [pickerValue, setPickerValue] = React.useState('Select User Type');
   
+    const onPress = () => {console.log(name)
+                          console.log(contactno)
+                          console.log(email)
+                          console.log(password)
+                        console.log(pickerValue)};
+
+    const onPressChange = () => {console.log("login presssed")}
+
+
     return (
-    <Text style={styles.baseText}>
+    <View style={styles.baseText}>
     <SafeAreaView>
-        <Text>
-            Sign UP
+        <Text style={styles.title}>
+           Sign UP
         </Text>
       <TextInput
       style={styles.input}
         label="Name"
         value={name}
+        placeholder="Name"
         onChangeText={name => setName(name)}
       />
       <TextInput
       style={styles.input}
         label="Contact No"
-        value={contactno}
+       value={contactno}
+       placeholder="Contact Info"
         onChangeText={contactno => setContactno(contactno)}
       />
+
+        <Text >
+           Select A User Type :
+        </Text>
+
+      <Picker
+        style= {styles.dropdown}
+        selectedValue={pickerValue}
+        onValueChange ={(itemValue) => setPickerValue(itemValue)}
+      >
+        <Picker.Item label="Traveller" value="Traveller" />
+        <Picker.Item label="Hotel Manager" value="HotelManager" />
+        <Picker.Item label="Blogger" value="Blogger" />
+        <Picker.Item label="Tour Guide" value="TourGuide" />
+        <Picker.Item label="Vehicle Owner" value="VehicleOwner" />
+
+      </Picker>
       <TextInput
       style={styles.input}
         label="Email"
         value={email}
+       placeholder="Email"
         onChangeText={email => setEmail(email)}
       />
       <TextInput
       style={styles.input}
         label="Password"
+        placeholder="Password"
         value={password}
         onChangeText={password => setPassword(password)}
       />
 
-        <Button
-              title="SignUp"
-              // buttons
-            />
-
+       <TouchableOpacity
+        style={styles.button}
+        onPress={onPress}
+      >
+        <Text>SignUp</Text>
+      </TouchableOpacity>
+          <View style={styles.footer}>
+              <Text>Already Registerd?</Text>
+              <TouchableOpacity
+              style={styles.button}
+              onPress={onPressChange}
+              >
+              <Text>Login</Text>
+              </TouchableOpacity>
+          </View>
       </SafeAreaView>
-      </Text>
+      </View>
+
     );
   };
 
   const styles = StyleSheet.create({
   
     baseText: {
+      flex: 1,
       marginTop: 40,
-     marginLeft: 80,
+     //marginLeft: 80,
      padding: 20,
      alignItems: 'center',
     },
+
+    button: {
+      alignItems: "center",
+      backgroundColor: "#00FFDD",
+      padding: 10
+    },
     
+    title: {
+      alignItems: "center",
+      marginBottom: 20,
+      padding: 10,
+      textAlign: 'center',
+    },
+
     input: {
       height: 40,
       width : 125,
@@ -66,6 +123,23 @@ const Register = () => {
       padding: 10,
       
     },
+
+    dropdown: {
+      height: 40,
+      width : 125,
+      margin: 22,
+      borderWidth: 2,
+      padding: 10,
+      
+    },
+
+    footer: {
+      marginTop: 40,
+     alignItems: 'flex-end',
+      padding: 10,
+      
+    },
+
   });
   
   export default Register;
