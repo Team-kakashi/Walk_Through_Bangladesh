942a78717050bb853d90bd3058883ed459c74b62
EushaMashfi <eushamashfi@gmail.com>
2022-01-22 21:34:58 +0600

    ui update

---
+++ src/infrastructure/theme/colors.js
@@ -0,0 +1,30 @@
+/** @format */
+
+export const colors = {
+  brand: {
+    primary: "#1CCA89",
+    secondary: "#6987A5",
+    muted: "#C9D9E8",
+  },
+  ui: {
+    primary: "#6987A5",
+    secondary: "#6987A5",
+    tertiary: "#E12D4B",
+    quaternary: "#FFFFFF",
+    disabled: "#C9D9E8",
+    error: "#E12D4B",
+    success: "#1CCA89",
+  },
+  bg: {
+    primary: "#F5F5F5",
+    secondary: "#FFFFFF",
+  },
+  text: {
+    primary: "#262626",
+    secondary: "#757575",
+    disabled: "#9C9C9C",
+    inverse: "#FFFFFF",
+    error: "#D0421B",
+    success: "#138000",
+  },
+};
