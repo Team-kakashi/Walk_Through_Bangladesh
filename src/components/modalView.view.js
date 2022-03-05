bf8721510c4d3a1420f5744c8afcd38a3c4970a7
EushaMashfi <eushamashfi@gmail.com>
2022-03-06 00:37:42 +0600

    update

--- src/components/modalView.view.js
+++ src/components/modalView.view.js
@@ -8,43 +8,25 @@ import {
   TouchableOpacityPicker,
 } from "./common.style";
 import { ModalPicker } from "./modalPicker";
-var selectingDropDown;
-var Title; 
-export var ItemChoise;
-export const ModalView = (props) => {
-  
-  Title = props.Title;
-  selectingDropDown = props.Array;
-  /*if(props.AC_option!=undefined){
-    selectingDropDown = props.AC_option;
-    console.log('ac ',selectingDropDown)
-    //Title = props.Title;
-  }
-  else{
-     selectingDropDown = props.Capacity;
-    console.log('capacity ',selectingDropDown);
-    //Title = props.Title;
-  }*/
-  
+
+export const ModalView = () => {
   const [chooseData, setChooseData] = useState(
-    Title
+    "Select Item..."
   );
+
   const [isModeVisible, setisModeVisible] = useState(false);
   const changeModalVisibility = (bool) => {
     setisModeVisible(bool);
   };
 
-  const setData = (selectingDropDown) => {
-    
-    props.PickerValue(selectingDropDown)
-    setChooseData(selectingDropDown);
+  const setData = (option) => {
+    setChooseData(option);
   };
 
   return (
     <SafeAreaViewContainer>
       <TouchableOpacityPicker
-        onPress={() =>{ changeModalVisibility(true);
-                      }}
+        onPress={() => changeModalVisibility(true)}
       >
         <Subtitle>{chooseData}</Subtitle>
       </TouchableOpacityPicker>
@@ -57,7 +39,6 @@ export const ModalView = (props) => {
         <ModalPicker
           changeModalVisibility={changeModalVisibility}
           setData={setData}
-          array={selectingDropDown}
         />
       </Modal>
     </SafeAreaViewContainer>
